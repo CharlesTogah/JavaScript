0bc56c6855caa78dc1eae5bed93b89f1f25f6837
Charles Damascus Togah <69472824+CharlesTogah@users.noreply.github.com>
2020-10-07 17:50:28 +0000

    second commit

--- jscript.js
+++ jscript.js
@@ -1,1 +1,19 @@
-new file
+let elements= document.getElementsByName('cssProperty');
+
+let div = document.getElementById('modify');
+
+function set()
+{
+	for (let i = 0; i < elements.length; i++){
+		
+		let cssProperty= elements[i].getAttribute('id');
+		
+		let cssValue= elements[i].value;
+		
+			div.style[cssProperty] = cssValue;
+	}
+}
+
+document.getElementById('set').addEventListener('click', set);
+
+
